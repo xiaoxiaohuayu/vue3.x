4a680bd2b917287ffde9546c3b59957b85024fd3
haozexiang <1521183864@qq.com>
2021-09-04 23:21:45 +0800

    初始化

---
+++ 3.x/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+export default {
+  name: 'App',
+  components: {HelloWorld}
+}
+</script>
---
+++ 3.x/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+<div>{{a}}</div>
+<button @click="a++">添加</button>
+{{msg}}
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  name: 'HelloWorld',
+  props: ['msg'],
+  setup(n,c){
+    let a = ref(0)
+    console.log(n,c,a)
+    return{
+      a
+    }
+  }
+}
+</script>
+<style scoped></style>
